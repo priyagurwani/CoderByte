ecf0f4315d6b48533d3cef498c46d04876f453f5
priyagurwani <84342895+priyagurwani@users.noreply.github.com>
2022-04-02 19:07:06 +0530

    Update 1_Check_Number_of_x_and_o.py

--- 1_Check_Number_of_x_and_o.py
+++ 1_Check_Number_of_x_and_o.py
@@ -3,5 +3,24 @@ def check(strParam):
     return True
   else:
     return False
- return strParam
+  return strParam
 print(check('xoxoxo'))
+
+# Or
+
+def check2(struser):
+    num_x=''
+    num_o=''
+    for i in struser:
+        if i=='x':
+            num_x+=i
+    for j in struser:
+        if j=='o':
+            num_o+=j
+
+    output=[True if len(num_x)==len(num_o) else False]
+    return output
+print(check2('xoxox'))
+print(check2('xxxooo'))
+
+
